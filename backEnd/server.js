7cab176ee4753dd0af6829bcef33722baa414a01
husseyp <harshpatel.hp817@gmail.com>
2023-11-06 13:17:48 +0530

    port changes

--- backEnd/server.js
+++ backEnd/server.js
@@ -36,4 +36,4 @@ app.get('/', (req, res) => {
     res.send('Welcome to my Project !!');
   });
 
-app.listen(process.env.PORT || "5000", () => console.log('Example app is listening on port 3000.'));
+app.listen(process.env.PORT || "3000", () => console.log('Example app is listening on port 3000.'));
